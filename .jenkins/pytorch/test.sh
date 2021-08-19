8771867306c8e702706bec7a1c8b07ff357ea3c5
Jiewen Tan <jwtan@fb.com>
2021-08-19 20:41:19 +0000

    Enable test_api IMethodTest in OSS
    Summary:
    This diff did the following few things to enable the tests:
    1. Exposed IMethod as TORCH_API.
    2. Linked torch_deploy to test_api if USE_DEPLOY == 1.
    3. Only run test_api::IMethodTest in the same environment/CI as test_deploy,
    which does some specialized configurations.
    
    Test Plan:
    ./build/bin/test_api --gtest_filter=IMethodTest.*
    
    ghstack-source-id: d2896e5a99fee623f7a6777d3716505f917068e1
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/63345

--- .jenkins/pytorch/test.sh
+++ .jenkins/pytorch/test.sh
@@ -497,14 +497,15 @@ elif [[ "${BUILD_ENVIRONMENT}" == *libtorch* ]]; then
   # TODO: run some C++ tests
   echo "no-op at the moment"
 elif [[ "${BUILD_ENVIRONMENT}" == *-test1 || "${JOB_BASE_NAME}" == *-test1 || "${SHARD_NUMBER}" == 1 ]]; then
-  if [[ "${BUILD_ENVIRONMENT}" == *linux-xenial-cuda11.1*-test1* ]]; then
-    test_torch_deploy
-  fi
   test_without_numpy
   install_torchvision
   test_python_shard1
   test_aten
 elif [[ "${BUILD_ENVIRONMENT}" == *-test2 || "${JOB_BASE_NAME}" == *-test2 || "${SHARD_NUMBER}" == 2 ]]; then
+  # test_libtorch (test_api) includes IMethodTest that can benefit from sharing the same configuration as test_deploy.
+  if [[ "${BUILD_ENVIRONMENT}" == *linux-xenial-cuda11.1*-test2* ]]; then
+    test_torch_deploy
+  fi
   install_torchvision
   test_python_shard2
   test_libtorch
