6afe13892321329ca03e7e29b75924ba8c7459d1
Karen Zhou <kazhou@fb.com>
2021-08-19 13:05:14 -0700

    [pruner] add support for pruning BatchNorm2d
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/63519
    
    If the pruner should be pruning biases along with weights, then if the model has BatchNorm2d following pruned Conv2d layers, then the corresponding channels of the BatchNorm must also be pruned.
    
    Specifically, they need to zeroed out, rather than fully removed, since in eager mode, the dimensions between layers need to be preserved.
    
    To do this, we add a pruning parametrization called `ZeroesParametrization` which zeroes out pruned channels, rather than removing them. We also add checks in the pruner's `prepare` and `step` method to warn the user to manually update the BatchNorm's pruned outputs with `manual_mask_update`.
    
    If `pruner.prune_bias` is True (i.e. we are pruning the entire output channel), then user API is as follows
    - user must add BN layers explicitly in `config`
        - this adds the BN layers to the pruner's module groups and attaches the  `ZeroesParametrization` and BiasHook during `prepare`
    - After `pruner.step` is called, the user must call `manual_mask_update(bn_layer, conv_pruned_idx)`, where `conv_pruned_idx` is the set of pruned indices of the preceding conv2d layer
    
    
    Note: `manual_mask_update` and pruning BN should only be necessary if the BatchNorm's bias is non-zero.
    ghstack-source-id: 136243062
    
    Differential Revision: [D30349855](https://our.internmc.facebook.com/intern/diff/D30349855/)

--- torch/ao/sparsity/experimental/pruner/base_pruner.py
+++ torch/ao/sparsity/experimental/pruner/base_pruner.py
@@ -1,4 +1,6 @@
 
+import copy
+import warnings
 import abc
 
 import torch
@@ -7,13 +9,22 @@ from torch.nn.utils import parametrize
 
 from torch.nn.modules.container import ModuleDict, ModuleList
 
-from .parametrization import PruningParametrization, ActivationReconstruction, BiasHook
+from .parametrization import PruningParametrization, ZeroesParametrization, ActivationReconstruction, BiasHook
 
-from torch.ao.sparsity import BaseSparsifier, fqn_to_module
+from torch.ao.sparsity import BaseSparsifier, module_to_fqn, fqn_to_module
 
-SUPPORTED_MODULES = {
+SUPPORTED_MODULES = {  # added to config if None given
     nn.Linear,
-    nn.Conv2d
+    nn.Conv2d,
+    nn.BatchNorm2d,  # will need manual update to match conv2d
+}
+
+NEEDS_MANUAL_UPDATE = {  # if model contains these layers, user must provide pruned indices
+    nn.BatchNorm2d
+}
+
+NEEDS_ZEROS = {  # these layers should have pruned indices zero-ed, not removed
+    nn.BatchNorm2d
 }
 
 
@@ -49,26 +60,77 @@ class BasePruner(BaseSparsifier):
             else:
                 module = config['module']
 
-            if getattr(module, 'mask', None) is None:
-                module.register_buffer('mask', torch.tensor(module.weight.shape[0]))
-            param = config.get('parametrization', PruningParametrization)
-            parametrize.register_parametrization(module, 'weight',
-                                                 param(module.mask),
-                                                 unsafe=True)
+            if not isinstance(module, tuple(NEEDS_ZEROS)):
+                # add pruning parametrization and forward hooks
+                if getattr(module, 'mask', None) is None:
+                    module.register_buffer('mask', torch.tensor(module.weight.shape[0]))
+                param = config.get('parametrization', PruningParametrization)
+                parametrize.register_parametrization(module, 'weight', param(module.mask), unsafe=True)
 
-            assert isinstance(module.parametrizations, ModuleDict)  # make mypy happy
-            assert isinstance(module.parametrizations.weight, ModuleList)
-            if isinstance(module, tuple(SUPPORTED_MODULES)):
-                self.activation_handles.append(module.register_forward_hook(
-                    ActivationReconstruction(module.parametrizations.weight[0])
-                ))
-            else:
-                raise NotImplementedError("This module type is not supported yet.")
+                assert isinstance(module.parametrizations, ModuleDict)  # make mypy happy
+                assert isinstance(module.parametrizations.weight, ModuleList)
+                if isinstance(module, tuple(SUPPORTED_MODULES)):
+                    self.activation_handles.append(module.register_forward_hook(
+                        ActivationReconstruction(module.parametrizations.weight[0])
+                    ))
+                else:
+                    raise NotImplementedError("This module type is not supported yet.")
+
+            else:  # needs zeros
+                if getattr(module, 'mask', None) is None:
+                    module.register_buffer('mask', torch.tensor(module.weight.shape[0]))
+                param = config.get('parametrization', ZeroesParametrization)
+                parametrize.register_parametrization(module, 'weight', param(module.mask), unsafe=True)
 
             if module.bias is not None:
                 module.register_parameter('_bias', nn.Parameter(module.bias.detach()))
                 module.bias = None
             self.bias_handles.append(module.register_forward_hook(BiasHook(module.parametrizations.weight[0], self.prune_bias)))
+
+    def prepare(self, model, config):
+        r"""Prepares a model, by adding the parametrizations and forward post-hooks.
+        Note::
+            The model is modified inplace. If you need to preserve the original
+            model, use copy.deepcopy.
+
+        Args:
+        - model [nn.Module]: model to configure. The model itself is not saved
+            but used for the state_dict saving / loading.
+        - config [list]: configuration elements could either be instances of
+            nn.Module or dict maps. The dicts must have a key 'module' with the
+            value being an instance of a nn.Module.
+        """
+        self.model = model  # TODO: Need to figure out how to load without this.
+        self.config = config
+
+        # If no config -- try getting all the supported layers
+        if self.config is None:
+            # Add all models to the config
+            self.config = []
+            stack = [model]
+            while stack:
+                module = stack.pop()
+                for name, child in module.named_children():
+                    if type(child) in SUPPORTED_MODULES:
+                        self.config.append(child)
+                    else:
+                        if type(child) in NEEDS_MANUAL_UPDATE and self.prune_bias:
+                            warnings.warn(f"Models with {type(child)} layers must have pruned outputs provided by user.")
+                        stack.append(child)
+
+        for module_config in self.config:
+            if isinstance(module_config, nn.Module):
+                module_config = {'module': module_config}
+            local_args = copy.deepcopy(self.defaults)
+            local_args.update(module_config)
+            module = local_args['module']
+            module_fqn = module_to_fqn(model, module)
+            if module_fqn and module_fqn[0] == '.':
+                module_fqn = module_fqn[1:]
+            local_args['fqn'] = module_fqn
+            self.module_groups.append(local_args)
+
+        self._prepare()
 
     def squash_mask(self, use_path=False, *args, **kwargs):
         for config in self.module_groups:
@@ -83,6 +145,25 @@ class BasePruner(BaseSparsifier):
             elif getattr(module._buffers, 'mask', None):
                 del module._buffers['mask']
             delattr(module, 'mask')
+
+    def manual_mask_update(self, module, pruned_outputs):
+        r"""Updates mask of module with user-provided pruned outputs"""
+        param = module.parametrizations.weight[0]
+        param.pruned_outputs.update(pruned_outputs)
+
+    def step(self, use_path=True):
+        if not self.enable_mask_update:
+            return
+        with torch.no_grad():
+            for config in self.module_groups:
+                if use_path:
+                    module = fqn_to_module(self.model, config['fqn'])
+                else:
+                    module = config['module']
+                if type(module) in NEEDS_MANUAL_UPDATE:
+                    warnings.warn(f"User must update mask of {type(module)} manually.")
+                else:
+                    self.update_mask(module, **config)
 
     @abc.abstractmethod
     def update_mask(self, layer, **kwargs):
